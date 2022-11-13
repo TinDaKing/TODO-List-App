361579a2a909ab52ee0642e9f546329fb187272f
TinDaKing <tranhoangtin3702@gmail.com>
2022-11-13 18:16:01 +0700

    js completed

--- js/task.js
+++ js/task.js
@@ -10,25 +10,49 @@ btnAddTaskEl.addEventListener('click', function() {
         return false
     }
 
+    let taskID = this.getAttribute('id')
     let tasks = getTaskFromLocaleStorage()
+    let task = {name:taskNameEl.value}
 
-    tasks.push({
-        name: taskNameEl.value
-    })
+    if (taskID==0||taskID){
+        tasks[taskID] = task
+        this.removeAttribute('id')
+    } else {
+        tasks.push(task)
+    }
+
     taskNameEl.value=''
     
     localStorage.setItem('tasks',JSON.stringify(tasks))
     renderTasks(tasks)
 })
 
+function editTask(id){
+    let tasks = getTaskFromLocaleStorage()
+    if(tasks.length>0){
+        taskNameEl.value=tasks[id].name
+        btnAddTaskEl.setAttribute('id',id)
+    }
+}
+
+function deleteTask(id){
+    let tasks = getTaskFromLocaleStorage()
+    tasks.splice(id, 1)
+    localStorage.setItem('tasks',JSON.stringify(tasks))
+    renderTasks(getTaskFromLocaleStorage())
+}
+
 function renderTasks(tasks = []){
     let content =''
-    tasks.forEach((task) => {
+    tasks.forEach((task,index) => {
         content+= `<li>
             <label>
                     <input type ="checkbox" name ="">
                     <p>${task.name}</p>
                     <span></span>
+                    <a href="#" onclick="editTask(${index})">Edit</a>
+                    
+                    <a href="#" onclick="deleteTask(${index})">Delete</a>
                 </label> 
         </li>`
     })
@@ -36,6 +60,7 @@ function renderTasks(tasks = []){
     content += '</ul>'
     document.querySelector('#result').innerHTML = content
 }
+
 
 function getTaskFromLocaleStorage(){
     return localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')):[]
